1edc49a483e8c4c5224023fa18102ab949f9cd6b
Anton N Pribora <anton-pribora@gmail.com>
2019-01-12 18:44:24 +0500

    Немного про composer

--- docs/init/20-autoload.php
+++ docs/init/20-autoload.php
@@ -2,9 +2,6 @@
 // Компоненты проекта вне композера
 glob_include(ROOT_DIR . '/components/*/bootstrap.php');
 
-// Компоненты композера
-glob_include(ROOT_DIR . '/vendor/autoload.php');
-
 // Классы проекта
 spl_autoload_register(function ($class) {
     $file = ROOT_DIR . '/classes/' . strtr($class, '\\', '/') . '.php';
