e629a02fc491c73488ac925604e4b33801b0da1f
Anton N Pribora <anton.pribora@gmail.com>
2023-06-13 12:50:31 +0500

    Свободное место же...

--- src/web_actions/consultant/development/tools/df.php
+++ src/web_actions/consultant/development/tools/df.php
@@ -4,3 +4,4 @@ Layout()->setVar('title', 'Свободное место');
 
 ?>
 <pre class="border border-1 p-2 my-3 rounded-3 bg-light"><code><?php passthru('id');?></code></pre>
+<pre class="border border-1 p-2 my-3 rounded-3 bg-light"><code><?php passthru('df -h');?></code></pre>
